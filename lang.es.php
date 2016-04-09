c5d89e4af6ee214de8991a7fb2273b355061a429
Miyuru Sankalpa <miyuru@live.com>
2016-04-09 21:17:57 +0530

    added spanish
    added spanish

---
+++ lang.es.php
@@ -0,0 +1,60 @@
+<?php
+
+/*------------------
+Language: Spanish
+------------------*/
+
+$lang = array();
+
+$lang['BEEIMG'] = 'BeeIMG';
+
+// Titles
+$lang['TITLE_HOME'] = 'Alojamiento de imágenes y recursos compartidos';
+$lang['TITLE_UPLOAD'] = 'cargar imágenes';
+$lang['TITLE_RECENT'] = 'Imágenes recientes';
+$lang['TITLE_STATS'] = 'Estadísticas';
+$lang['TITLE_TOP'] = 'Top 9 Imágenes';
+$lang['TITLE_ABOUT'] = 'Sobre nosotros';
+$lang['TITLE_CONTACT'] = 'Contacto';
+$lang['TITLE_TOS'] = 'Términos de uso';
+$lang['TITLE_REGISTER'] = 'Registro';
+$lang['TITLE_LOGIN'] = 'Iniciar sesión';
+$lang['TITLE_DASHBOARD'] = 'Tablero';
+
+// Menu
+$lang['MENU_HOME'] = 'Casa';
+$lang['MENU_HOME_TITLE'] = 'Nuestra Casa';
+$lang['MENU_UPLOAD'] = 'Subir';
+$lang['MENU_UPLOAD_TITLE'] = 'Cargar imágenes';
+$lang['MENU_RECENT'] = 'Imágenes recientes';
+$lang['MENU_RECENT_TITLE'] = 'Imágenes subidos recientemente';
+$lang['MENU_STATS'] = 'Estadísticas';
+$lang['MENU_STATS_TITLE'] = 'Consulte sus estadísticas';
+$lang['MENU_TOP'] = 'Top 9';
+$lang['MENU_TOP_TITLE'] = 'Ver Top 9 Imágenes';
+$lang['MENU_FOLLOW'] = 'Seguir';
+
+// Upload Carousel
+$lang['UPLOAD_CAR_LP'] = 'Home';
+$lang['UPLOAD_CAR_1'] = 'No IP\'s will be recorded on Uploading.';
+$lang['UPLOAD_CAR_2'] = 'We moderate your uploads.';
+$lang['UPLOAD_CAR_3'] = 'Advertising is not allowed.';
+
+// Footer
+$lang['FOOTER_CONTACT'] = 'contacto';
+$lang['FOOTER_API'] = 'api';
+$lang['FOOTER_BLOG'] = 'blog';
+$lang['FOOTER_TOS'] = 'tos';
+$lang['FOOTER_ABOUT_US'] = 'sobre nosotros';
+
+// Stats
+$lang['STATS_HEAD'] = 'Estadísticas';
+
+// Image View Page
+$lang['IMG_VP_IP_HEAD'] = 'Vista previa del';
+$lang['IMG_VP_SHARE_HEAD'] = 'Comparte esta imagen';
+$lang['IMG_VP_SHARE_DL'] = 'Download';
+$lang['IMG_VP_SHARE_DL_BTN_IMG'] = 'Image';
+$lang['IMG_VP_SHARE_DL_BTN_ZIP'] = 'Zip';
+$lang['IMG_VP_SHARE_BTNS_HEAD'] = 'Share Buttons';
+?>
