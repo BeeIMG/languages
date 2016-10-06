ddc092ac643d03e36f383090ae8b4c48612dade5
Danny van der Sluijs <dvandersluijs@monitorlinq.com>
2016-10-06 21:38:09 +0200

    Add Dutch translation file

---
+++ lang.nl.php
@@ -0,0 +1,60 @@
+<?php
+
+/*------------------
+Language: English
+------------------*/
+
+$lang = array();
+
+$lang['BEEIMG'] = 'BeeIMG';
+
+// Titles
+$lang['TITLE_HOME'] = 'Free Image Hosting and Sharing';
+$lang['TITLE_UPLOAD'] = 'Upload afbeeldingen';
+$lang['TITLE_RECENT'] = 'Recente afbeeldingen';
+$lang['TITLE_STATS'] = 'Statistieken';
+$lang['TITLE_TOP'] = 'Top 9 afbeeldingen';
+$lang['TITLE_ABOUT'] = 'Over ons';
+$lang['TITLE_CONTACT'] = 'Contact';
+$lang['TITLE_TOS'] = 'Voorwaarden';
+$lang['TITLE_REGISTER'] = 'Registreer';
+$lang['TITLE_LOGIN'] = 'Login';
+$lang['TITLE_DASHBOARD'] = 'Dashboard';
+
+// Menu
+$lang['MENU_HOME'] = 'Home';
+$lang['MENU_HOME_TITLE'] = 'Home';
+$lang['MENU_UPLOAD'] = 'Upload';
+$lang['MENU_UPLOAD_TITLE'] = 'Upload afbeeldingen';
+$lang['MENU_RECENT'] = 'Recente afbeeldingen';
+$lang['MENU_RECENT_TITLE'] = 'Recentelijk geuploade afbeeldingen';
+$lang['MENU_STATS'] = 'Statistieken';
+$lang['MENU_STATS_TITLE'] = 'Toon onze statistieken';
+$lang['MENU_TOP'] = 'Top 9';
+$lang['MENU_TOP_TITLE'] = 'Toon top 9 afbeeldingen';
+$lang['MENU_FOLLOW'] = 'Volg';
+
+// Upload Carousel
+$lang['UPLOAD_CAR_LP'] = 'Home';
+$lang['UPLOAD_CAR_1'] = 'Er worden geen IP-adressen vast gelegd tijdens uploaden.';
+$lang['UPLOAD_CAR_2'] = 'Wij modereren je uploads.';
+$lang['UPLOAD_CAR_3'] = 'Adverteren is niet toegestaan.';
+
+// Footer
+$lang['FOOTER_CONTACT'] = 'contact';
+$lang['FOOTER_API'] = 'api';
+$lang['FOOTER_BLOG'] = 'blog';
+$lang['FOOTER_TOS'] = 'voorwaarden';
+$lang['FOOTER_ABOUT_US'] = 'over ons';
+
+// Stats
+$lang['STATS_HEAD'] = 'Statistieken';
+
+// Image View Page
+$lang['IMG_VP_IP_HEAD'] = 'Afbeelding preview van';
+$lang['IMG_VP_SHARE_HEAD'] = 'Deel deze afbeelding';
+$lang['IMG_VP_SHARE_DL'] = 'Download';
+$lang['IMG_VP_SHARE_DL_BTN_IMG'] = 'Afbeelding';
+$lang['IMG_VP_SHARE_DL_BTN_ZIP'] = 'Zip';
+$lang['IMG_VP_SHARE_BTNS_HEAD'] = 'Deel knoppen';
+?>
