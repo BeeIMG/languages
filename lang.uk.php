080367adc75b88a84f790c14748acb12a13fc588
Taras Panychok <pish4@mail.ua>
2016-10-06 21:24:11 +0300

    Add Ukrainian translation

---
+++ lang.uk.php
@@ -0,0 +1,51 @@
+<?php
+/*------------------
+Language: Ukrainian
+------------------*/
+$lang = array();
+$lang['BEEIMG'] = 'BeeIMG';
+// Titles
+$lang['TITLE_HOME'] = 'Безкоштовний хостинг зображень';
+$lang['TITLE_UPLOAD'] = 'Завантажити зображення';
+$lang['TITLE_RECENT'] = 'Останні зображення';
+$lang['TITLE_STATS'] = 'Статистика';
+$lang['TITLE_TOP'] = 'Топ 9 зображень';
+$lang['TITLE_ABOUT'] = 'Про нас';
+$lang['TITLE_CONTACT'] = 'Зв’язатись';
+$lang['TITLE_TOS'] = 'Умови використання;
+$lang['TITLE_REGISTER'] = 'Реєстрація';
+$lang['TITLE_LOGIN'] = 'Вхід';
+$lang['TITLE_DASHBOARD'] = 'Панель';
+// Menu
+$lang['MENU_HOME'] = 'Головна';
+$lang['MENU_HOME_TITLE'] = 'Наш дім';
+$lang['MENU_UPLOAD'] = 'Завантажити';
+$lang['MENU_UPLOAD_TITLE'] = 'Завантажити зображення';
+$lang['MENU_RECENT'] = 'Останні зображення';
+$lang['MENU_RECENT_TITLE'] = 'Останні завантажені зображення';
+$lang['MENU_STATS'] = 'Статистика';
+$lang['MENU_STATS_TITLE'] = 'Переглянути статистику';
+$lang['MENU_TOP'] = 'Топ 9';
+$lang['MENU_TOP_TITLE'] = 'Переглянути топ 9 зображень';
+$lang['MENU_FOLLOW'] = 'Стежити';
+// Upload Carousel
+$lang['UPLOAD_CAR_LP'] = 'Головна';
+$lang['UPLOAD_CAR_1'] = 'Ваша IP адреса не буде збереженою при завантаженні.';
+$lang['UPLOAD_CAR_2'] = 'Ми модеруємо ваші завантаження.';
+$lang['UPLOAD_CAR_3'] = 'Рекламні матеріали заборонені.';
+// Footer
+$lang['FOOTER_CONTACT'] = 'зв’язатись';
+$lang['FOOTER_API'] = 'api';
+$lang['FOOTER_BLOG'] = 'блог';
+$lang['FOOTER_TOS'] = 'tos';
+$lang['FOOTER_ABOUT_US'] = 'про нас';
+// Stats
+$lang['STATS_HEAD'] = 'Статистика';
+// Image View Page
+$lang['IMG_VP_IP_HEAD'] = 'Попередній перегляд';
+$lang['IMG_VP_SHARE_HEAD'] = 'Поділитись цим зображенням';
+$lang['IMG_VP_SHARE_DL'] = 'Завантажити';
+$lang['IMG_VP_SHARE_DL_BTN_IMG'] = 'Зображення';
+$lang['IMG_VP_SHARE_DL_BTN_ZIP'] = 'Zip';
+$lang['IMG_VP_SHARE_BTNS_HEAD'] = 'Поділитись';
+?>
