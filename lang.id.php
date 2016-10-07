79a9734122d7c624d7b75ed3bd4ad446a254d3a6
Afif Hendrawan <afif.hendrawan@gmail.com>
2016-10-07 10:56:56 +0700

    Bahasa Indonesia

---
+++ lang.id.php
@@ -0,0 +1,60 @@
+<?php
+
+/*------------------
+Language: Bahasa Indonesia
+------------------*/
+
+$lang = array();
+
+$lang['BEEIMG'] = 'BeeIMG';
+
+// Titles
+$lang['TITLE_HOME'] = 'Hosting dan Berbagi Gambar Gratis';
+$lang['TITLE_UPLOAD'] = 'Unggah Gambar';
+$lang['TITLE_RECENT'] = 'Gambar Terbaru';
+$lang['TITLE_STATS'] = 'Status';
+$lang['TITLE_TOP'] = '9 Gambar Teratas';
+$lang['TITLE_ABOUT'] = 'Tentang Kami';
+$lang['TITLE_CONTACT'] = 'Kontak';
+$lang['TITLE_TOS'] = 'Ketentuan';
+$lang['TITLE_REGISTER'] = 'Daftar';
+$lang['TITLE_LOGIN'] = 'Masuk';
+$lang['TITLE_DASHBOARD'] = 'Dasbor';
+
+// Menu
+$lang['MENU_HOME'] = 'Home';
+$lang['MENU_HOME_TITLE'] = 'Home Kami'; //Our Home
+$lang['MENU_UPLOAD'] = 'Unggah';
+$lang['MENU_UPLOAD_TITLE'] = 'Unggah Gambar';
+$lang['MENU_RECENT'] = 'Gambar Terbaru';
+$lang['MENU_RECENT_TITLE'] = 'Gambar yang Baru di Unggah'; //Recently Uploaded Images
+$lang['MENU_STATS'] = 'Status';
+$lang['MENU_STATS_TITLE'] = 'Lihat Status Kami';
+$lang['MENU_TOP'] = '9 Teratas';
+$lang['MENU_TOP_TITLE'] = 'Lihat 9 Gambar Teratas';
+$lang['MENU_FOLLOW'] = 'Ikuti';
+
+// Upload Carousel
+$lang['UPLOAD_CAR_LP'] = 'Home';
+$lang['UPLOAD_CAR_1'] = 'Tidak IP yang akan direkam pada saat proses Mengunggah.';
+$lang['UPLOAD_CAR_2'] = 'Kami mengelola unggahan Anda.';
+$lang['UPLOAD_CAR_3'] = 'Pengiklanan tidak di izinkan.';
+
+// Footer
+$lang['FOOTER_CONTACT'] = 'kontak';
+$lang['FOOTER_API'] = 'api';
+$lang['FOOTER_BLOG'] = 'blog';
+$lang['FOOTER_TOS'] = 'ketentuan';
+$lang['FOOTER_ABOUT_US'] = 'tentang kami';
+
+// Stats
+$lang['STATS_HEAD'] = 'Status';
+
+// Image View Page
+$lang['IMG_VP_IP_HEAD'] = 'Gambar Tinjau dari';
+$lang['IMG_VP_SHARE_HEAD'] = 'Bagikan gambar ini';
+$lang['IMG_VP_SHARE_DL'] = 'Unduh';
+$lang['IMG_VP_SHARE_DL_BTN_IMG'] = 'Gambar';
+$lang['IMG_VP_SHARE_DL_BTN_ZIP'] = 'Zip';
+$lang['IMG_VP_SHARE_BTNS_HEAD'] = 'Tombol Bagikan';
+?>
