39466b9265b3877e987bf4b77329e81fe1e2450e
p0v1n0m <p0v1n0m@gmail.com>
2016-10-07 12:35:33 +0200

    Added Belarusian translation

---
+++ lang.by.php
@@ -0,0 +1,60 @@
+<?php
+
+/*------------------
+Language: Belarusian
+------------------*/
+
+$lang = array();
+
+$lang['BEEIMG'] = 'BeeIMG';
+
+// Titles
+$lang['TITLE_HOME'] = 'Бясплатны хостынг малюнкаў';
+$lang['TITLE_UPLOAD'] = 'Загрузка малюнкаў';
+$lang['TITLE_RECENT'] = 'Апошнія малюнкі';
+$lang['TITLE_STATS'] = 'Статыстыка';
+$lang['TITLE_TOP'] = 'Top 9 малюнкаў';
+$lang['TITLE_ABOUT'] = 'Пра нас';
+$lang['TITLE_CONTACT'] = 'Кантакт';
+$lang['TITLE_TOS'] = 'Умовы прадастаўлення паслуг';
+$lang['TITLE_REGISTER'] = 'Рэгістрацыя';
+$lang['TITLE_LOGIN'] = 'Уваход';
+$lang['TITLE_DASHBOARD'] = 'Панэль';
+
+// Menu
+$lang['MENU_HOME'] = 'Галоўная';
+$lang['MENU_HOME_TITLE'] = 'Наш дом';
+$lang['MENU_UPLOAD'] = 'Загрузіць';
+$lang['MENU_UPLOAD_TITLE'] = 'Загрузка малюнкаў';
+$lang['MENU_RECENT'] = 'Апошнія малюнкі';
+$lang['MENU_RECENT_TITLE'] = 'Апошнія загружаныя малюнкі';
+$lang['MENU_STATS'] = 'Статыстыка';
+$lang['MENU_STATS_TITLE'] = 'Паглядзець нашу статыстыку';
+$lang['MENU_TOP'] = 'Top 9';
+$lang['MENU_TOP_TITLE'] = 'Паглядзець Top 9 малюнкаў';
+$lang['MENU_FOLLOW'] = 'Сачыць';
+
+// Upload Carousel
+$lang['UPLOAD_CAR_LP'] = 'Галоўная';
+$lang['UPLOAD_CAR_1'] = 'IP-адрасы не будуць запісаны пры загрузцы.';
+$lang['UPLOAD_CAR_2'] = 'Мы правяраем вашыя загрузкі.';
+$lang['UPLOAD_CAR_3'] = 'Рэклама не дапускаецца.';
+
+// Footer
+$lang['FOOTER_CONTACT'] = 'кантакт';
+$lang['FOOTER_API'] = 'api';
+$lang['FOOTER_BLOG'] = 'блог';
+$lang['FOOTER_TOS'] = 'tos';
+$lang['FOOTER_ABOUT_US'] = 'пра нас';
+
+// Stats
+$lang['STATS_HEAD'] = 'Статыстыка';
+
+// Image View Page
+$lang['IMG_VP_IP_HEAD'] = 'Прадпрагляд малюнкаў';
+$lang['IMG_VP_SHARE_HEAD'] = 'Падзяліцца гэтым малюнкам';
+$lang['IMG_VP_SHARE_DL'] = 'Спампаваць';
+$lang['IMG_VP_SHARE_DL_BTN_IMG'] = 'Малюнак';
+$lang['IMG_VP_SHARE_DL_BTN_ZIP'] = 'Zip';
+$lang['IMG_VP_SHARE_BTNS_HEAD'] = 'Падзяліцца';
+?>
