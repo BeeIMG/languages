8a8e16c135f963a84705406a0ce9846d6b410845
root <root@XPS13-GOLD.localdomain>
2016-10-08 00:03:30 +1100

    add Traditional Chinese translate

---
+++ lang.zh.php
@@ -0,0 +1,61 @@
+<?php
+
+/*------------------
+Language: English
+------------------*/
+
+$lang = array();
+
+$lang['BEEIMG'] = 'BeeIMG';
+
+// Titles
+$lang['TITLE_HOME'] = '免費圖片寄存及分享';
+$lang['TITLE_UPLOAD'] = '上載圖片';
+$lang['TITLE_RECENT'] = '最近圖片';
+$lang['TITLE_STATS'] = '統計';
+$lang['TITLE_TOP'] = '最熱門圖片';
+$lang['TITLE_ABOUT'] = '有關我們';
+$lang['TITLE_CONTACT'] = '聯絡';
+$lang['TITLE_TOS'] = '服務條款';
+$lang['TITLE_REGISTER'] = '注冊';
+$lang['TITLE_LOGIN'] = '登入';
+$lang['TITLE_DASHBOARD'] = '一覽';
+
+// Menu
+$lang['MENU_HOME'] = '首頁';
+$lang['MENU_HOME_TITLE'] = '我們的首頁';
+$lang['MENU_UPLOAD'] = '上載';
+$lang['MENU_UPLOAD_TITLE'] = '上載圖片';
+$lang['MENU_RECENT'] = '最近圖片';
+$lang['MENU_RECENT_TITLE'] = '最近上載圖片';
+$lang['MENU_STATS'] = '統計';
+$lang['MENU_STATS_TITLE'] = '我們的統計';
+$lang['MENU_TOP'] = '最熱門圖片';
+$lang['MENU_TOP_TITLE'] = '觀看最熱門圖片';
+$lang['MENU_FOLLOW'] = '追蹤';
+
+// Upload Carousel
+$lang['UPLOAD_CAR_LP'] = '首頁';
+$lang['UPLOAD_CAR_1'] = 'IP地址將不會被紀錄。.';
+$lang['UPLOAD_CAR_2'] = '我們將會審核你上傳的內容。';
+$lang['UPLOAD_CAR_3'] = '嚴禁廣告';
+
+// Footer
+$lang['FOOTER_CONTACT'] = '聯絡';
+$lang['FOOTER_API'] = 'API';
+$lang['FOOTER_BLOG'] = 'blog';
+$lang['FOOTER_TOS'] = '服務條款';
+$lang['FOOTER_ABOUT_US'] = '有關我們';
+
+// Stats
+$lang['STATS_HEAD'] = '統計';
+
+// Image View Page
+$lang['IMG_VP_IP_HEAD'] = '圖片預覽';
+$lang['IMG_VP_SHARE_HEAD'] = '分享圖片';
+$lang['IMG_VP_SHARE_DL'] = '下載';
+$lang['IMG_VP_SHARE_DL_BTN_IMG'] = '圖片';
+$lang['IMG_VP_SHARE_DL_BTN_ZIP'] = 'Zip';
+$lang['IMG_VP_SHARE_BTNS_HEAD'] = '分享';
+?>
+
