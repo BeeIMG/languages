855433d98bb33258a3e401629402ece749c5cbf3
David Carr <dave@daveismyname.com>
2016-10-07 02:26:12 +0100

    Italian language

---
+++ lang.it.php
@@ -0,0 +1,51 @@
+<?php
+/*------------------
+Language: Italian
+------------------*/
+$lang = array();
+$lang['BEEIMG'] = 'BeeIMG';
+// Titoli
+$lang['TITLE_HOME'] = 'Free Image Hosting e condivisione';
+$lang['TITLE_UPLOAD'] = 'Caricare immagini';
+$lang['TITLE_RECENT'] = 'Immagini recenti';
+$lang['TITLE_STATS'] = 'Statistiche';
+$lang['TITLE_TOP'] = 'Top 9 immagini';
+$lang['TITLE_ABOUT'] = 'Chi Siamo';
+$lang['TITLE_CONTACT'] = 'Rivolga';
+$lang['TITLE_TOS'] = 'Termini di servizio';
+$lang['TITLE_REGISTER'] = 'Registrare';
+$lang['TITLE_LOGIN'] = 'Account di accesso';
+$lang['TITLE_DASHBOARD'] = 'Cruscotto';
+// Dal menu
+$lang['MENU_HOME'] = 'Casa';
+$lang['MENU_HOME_TITLE'] = 'La nostra casa';
+$lang['MENU_UPLOAD'] = 'Caricare';
+$lang['MENU_UPLOAD_TITLE'] = 'Caricare immagini';
+$lang['MENU_RECENT'] = 'Immagini recenti';
+$lang['MENU_RECENT_TITLE'] = 'Recentemente le immagini caricate';
+$lang['MENU_STATS'] = 'Statistiche';
+$lang['MENU_STATS_TITLE'] = 'Visualizza le nostre statistiche';
+$lang['MENU_TOP'] = 'Top 9';
+$lang['MENU_TOP_TITLE'] = 'Visualizza Top 9 immagini';
+$lang['MENU_FOLLOW'] = 'Sequire';
+// Caricare il carosello
+$lang['UPLOAD_CAR_LP'] = 'Casa';
+$lang['UPLOAD_CAR_1'] = 'Nessun mensaje sarà registrato il caricament.';
+$lang['UPLOAD_CAR_2'] = 'Moderiamo i caricamenti.';
+$lang['UPLOAD_CAR_3'] = 'La pubblicità non è consentita.';
+// Piè di pagina
+$lang['FOOTER_CONTACT'] = 'Rivolga';
+$lang['FOOTER_API'] = 'api';
+$lang['FOOTER_BLOG'] = 'blog';
+$lang['FOOTER_TOS'] = 'tos';
+$lang['FOOTER_ABOUT_US'] = 'Chi Siamo';
+// Statistiche
+$lang['STATS_HEAD'] = 'Statistiche';
+// Pagina di visualizzazione immagini
+$lang['IMG_VP_IP_HEAD'] = 'Immagine di anteprima del';
+$lang['IMG_VP_SHARE_HEAD'] = 'Condividi questa immagine';
+$lang['IMG_VP_SHARE_DL'] = 'Scarica';
+$lang['IMG_VP_SHARE_DL_BTN_IMG'] = 'Immagine';
+$lang['IMG_VP_SHARE_DL_BTN_ZIP'] = 'Zip';
+$lang['IMG_VP_SHARE_BTNS_HEAD'] = 'I pulsanti di condivisione';
+?>
