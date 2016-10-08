7e83f6b6cd6a8416ddc4852deecc3a7b7baf7462
David Maicher <mail@dmaicher.de>
2016-10-08 19:04:02 +0200

    add German translations

---
+++ lang.de.php
@@ -0,0 +1,60 @@
+<?php
+
+/*------------------
+Language: German
+------------------*/
+
+$lang = array();
+
+$lang['BEEIMG'] = 'BeeIMG';
+
+// Titles
+$lang['TITLE_HOME'] = 'Kostenlos Bilder hochladen und teilen';
+$lang['TITLE_UPLOAD'] = 'Bilder hochladen';
+$lang['TITLE_RECENT'] = 'Zuletzt hochgeladene Bilder';
+$lang['TITLE_STATS'] = 'Statistiken';
+$lang['TITLE_TOP'] = 'Top 9 Bilder';
+$lang['TITLE_ABOUT'] = 'Über uns';
+$lang['TITLE_CONTACT'] = 'Kontakt';
+$lang['TITLE_TOS'] = 'AGB\'s';
+$lang['TITLE_REGISTER'] = 'Registrieren';
+$lang['TITLE_LOGIN'] = 'Login';
+$lang['TITLE_DASHBOARD'] = 'Dashboard';
+
+// Menu
+$lang['MENU_HOME'] = 'Überblick';
+$lang['MENU_HOME_TITLE'] = 'Unser Überblick';
+$lang['MENU_UPLOAD'] = 'Hochladen';
+$lang['MENU_UPLOAD_TITLE'] = 'Bilder hochladen';
+$lang['MENU_RECENT'] = 'Zuletzt hochgeladen';
+$lang['MENU_RECENT_TITLE'] = 'Zuletzt hochgeladene Bilder';
+$lang['MENU_STATS'] = 'Statistiken';
+$lang['MENU_STATS_TITLE'] = 'Schaue unsere Statistiken an';
+$lang['MENU_TOP'] = 'Top 9';
+$lang['MENU_TOP_TITLE'] = 'Top 9 Bilder anschauen';
+$lang['MENU_FOLLOW'] = 'Folgen';
+
+// Upload Carousel
+$lang['UPLOAD_CAR_LP'] = 'Überblick';
+$lang['UPLOAD_CAR_1'] = 'Es werden beim Upload keine IP Adressen gespeichert';
+$lang['UPLOAD_CAR_2'] = 'Wir überprüfen und moderieren Bilder nach dem Hochladen';
+$lang['UPLOAD_CAR_3'] = 'Werbung ist nicht erlaubt.';
+
+// Footer
+$lang['FOOTER_CONTACT'] = 'Kontakt';
+$lang['FOOTER_API'] = 'API';
+$lang['FOOTER_BLOG'] = 'Blog';
+$lang['FOOTER_TOS'] = 'AGB';
+$lang['FOOTER_ABOUT_US'] = 'Über uns';
+
+// Stats
+$lang['STATS_HEAD'] = 'Statistiken';
+
+// Image View Page
+$lang['IMG_VP_IP_HEAD'] = 'Bilder Vorschau von';
+$lang['IMG_VP_SHARE_HEAD'] = 'Dieses Bild teilen';
+$lang['IMG_VP_SHARE_DL'] = 'Herunterladen';
+$lang['IMG_VP_SHARE_DL_BTN_IMG'] = 'Bild';
+$lang['IMG_VP_SHARE_DL_BTN_ZIP'] = 'Zip';
+$lang['IMG_VP_SHARE_BTNS_HEAD'] = 'Buttons zum Teilen';
+?>
