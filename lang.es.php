329279a89977ef06d9a9a0096605fdeea0dc55c9
jslirola <newboy_master@hotmail.com>
2016-10-08 21:05:27 +0200

    Fix litte errors.

--- lang.es.php
+++ lang.es.php
@@ -9,36 +9,36 @@ $lang = array();
 $lang['BEEIMG'] = 'BeeIMG';
 
 // Titles
-$lang['TITLE_HOME'] = 'Alojamiento de imágenes y recursos compartidos';
-$lang['TITLE_UPLOAD'] = 'cargar imágenes';
-$lang['TITLE_RECENT'] = 'Imágenes recientes';
+$lang['TITLE_HOME'] = 'Alojamiento gratuito de Imágenes y Recursos Compartidos';
+$lang['TITLE_UPLOAD'] = 'Cargar Imágenes';
+$lang['TITLE_RECENT'] = 'Imágenes Recientes';
 $lang['TITLE_STATS'] = 'Estadísticas';
 $lang['TITLE_TOP'] = 'Top 9 Imágenes';
-$lang['TITLE_ABOUT'] = 'Sobre nosotros';
+$lang['TITLE_ABOUT'] = 'Sobre Nosotros';
 $lang['TITLE_CONTACT'] = 'Contacto';
-$lang['TITLE_TOS'] = 'Términos de uso';
+$lang['TITLE_TOS'] = 'Términos de Uso';
 $lang['TITLE_REGISTER'] = 'Registro';
-$lang['TITLE_LOGIN'] = 'Iniciar sesión';
+$lang['TITLE_LOGIN'] = 'Iniciar Sesión';
 $lang['TITLE_DASHBOARD'] = 'Tablero';
 
 // Menu
 $lang['MENU_HOME'] = 'Inicio';
 $lang['MENU_HOME_TITLE'] = 'Inicio';
 $lang['MENU_UPLOAD'] = 'Subir';
-$lang['MENU_UPLOAD_TITLE'] = 'Cargar imágenes';
+$lang['MENU_UPLOAD_TITLE'] = 'Cargar Imágenes';
 $lang['MENU_RECENT'] = 'Imágenes recientes';
-$lang['MENU_RECENT_TITLE'] = 'Imágenes subidos recientemente';
+$lang['MENU_RECENT_TITLE'] = 'Imágenes Subidas Recientemente';
 $lang['MENU_STATS'] = 'Estadísticas';
-$lang['MENU_STATS_TITLE'] = 'Consulte sus estadísticas';
+$lang['MENU_STATS_TITLE'] = 'Consulte sus Estadísticas';
 $lang['MENU_TOP'] = 'Top 9';
 $lang['MENU_TOP_TITLE'] = 'Ver Top 9 Imágenes';
 $lang['MENU_FOLLOW'] = 'Seguir';
 
 // Upload Carousel
 $lang['UPLOAD_CAR_LP'] = 'Inicio';
-$lang['UPLOAD_CAR_1'] = 'Ningúna IP será registrada en la carga.';
-$lang['UPLOAD_CAR_2'] = 'Moderamos sus cargas.';
-$lang['UPLOAD_CAR_3'] = 'La publicidad no esta permitida.';
+$lang['UPLOAD_CAR_1'] = 'Ninguna IP será registrada en la subida.';
+$lang['UPLOAD_CAR_2'] = 'Moderamos sus subidas.';
+$lang['UPLOAD_CAR_3'] = 'La publicidad no está permitida.';
 
 // Footer
 $lang['FOOTER_CONTACT'] = 'contacto';
@@ -52,9 +52,9 @@ $lang['STATS_HEAD'] = 'Estadísticas';
 
 // Image View Page
 $lang['IMG_VP_IP_HEAD'] = 'Vista previa del';
-$lang['IMG_VP_SHARE_HEAD'] = 'Comparte esta imagen';
+$lang['IMG_VP_SHARE_HEAD'] = 'Compartir esta imagen';
 $lang['IMG_VP_SHARE_DL'] = 'Descargar';
 $lang['IMG_VP_SHARE_DL_BTN_IMG'] = 'Imagen';
 $lang['IMG_VP_SHARE_DL_BTN_ZIP'] = 'Zip';
-$lang['IMG_VP_SHARE_BTNS_HEAD'] = 'Botones de compartir';
+$lang['IMG_VP_SHARE_BTNS_HEAD'] = 'Botones de Compartir';
 ?>
