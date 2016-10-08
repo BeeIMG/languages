28b9229b8c091bccb481ef4367562462fe276d98
Dasio <d.mikus@gmail.com>
2016-10-08 14:47:57 +0200

    Added Slovak translation

---
+++ lang.sk.php
@@ -0,0 +1,60 @@
+<?php
+
+/*------------------
+Language: Slovak
+------------------*/
+
+$lang = array();
+
+$lang['BEEIMG'] = 'BeeIMG';
+
+// Titles
+$lang['TITLE_HOME'] = 'Free Image Hosting and Sharing';
+$lang['TITLE_UPLOAD'] = 'Nahrať Obrázky';
+$lang['TITLE_RECENT'] = 'Nedávne Obrázky';
+$lang['TITLE_STATS'] = 'Štatistiky';
+$lang['TITLE_TOP'] = 'Top 9 Obrázkov';
+$lang['TITLE_ABOUT'] = 'O Nás';
+$lang['TITLE_CONTACT'] = 'Kontakt';
+$lang['TITLE_TOS'] = 'Podmienky služby';
+$lang['TITLE_REGISTER'] = 'Registrácia';
+$lang['TITLE_LOGIN'] = 'Prihlásenie';
+$lang['TITLE_DASHBOARD'] = 'Nástenka';
+
+// Menu
+$lang['MENU_HOME'] = 'Domov';
+$lang['MENU_HOME_TITLE'] = 'Náš Domov';
+$lang['MENU_UPLOAD'] = 'Nahrať';
+$lang['MENU_UPLOAD_TITLE'] = 'Nahrať Obrázky';
+$lang['MENU_RECENT'] = 'Nedávne Obrázky';
+$lang['MENU_RECENT_TITLE'] = 'Naposledy nahrané obrázky';
+$lang['MENU_STATS'] = 'Štatistika';
+$lang['MENU_STATS_TITLE'] = 'Pozrite si naše Štatistiky';
+$lang['MENU_TOP'] = 'Top 9';
+$lang['MENU_TOP_TITLE'] = 'Pozri si Top 9 Obrázkov';
+$lang['MENU_FOLLOW'] = 'Sledovať';
+
+// Upload Carousel
+$lang['UPLOAD_CAR_LP'] = 'Domov';
+$lang['UPLOAD_CAR_1'] = 'Žiadna IP nebude zaznamenaná pri nahravaní';
+$lang['UPLOAD_CAR_2'] = 'Kontrolujeme vaše nahrané súbory';
+$lang['UPLOAD_CAR_3'] = 'Širenie reklamy nie je dovolené.';
+
+// Footer
+$lang['FOOTER_CONTACT'] = 'kontakt';
+$lang['FOOTER_API'] = 'api';
+$lang['FOOTER_BLOG'] = 'blog';
+$lang['FOOTER_TOS'] = 'tos';
+$lang['FOOTER_ABOUT_US'] = 'o nás';
+
+// Stats
+$lang['STATS_HEAD'] = 'Štatistiky';
+
+// Image View Page
+$lang['IMG_VP_IP_HEAD'] = 'Náhľad obrázka';
+$lang['IMG_VP_SHARE_HEAD'] = 'Zdielať tento obrázok';
+$lang['IMG_VP_SHARE_DL'] = 'Stiahnuť';
+$lang['IMG_VP_SHARE_DL_BTN_IMG'] = 'Obrázok';
+$lang['IMG_VP_SHARE_DL_BTN_ZIP'] = 'Zip';
+$lang['IMG_VP_SHARE_BTNS_HEAD'] = 'Tlačítka pre zdieľanie';
+?>
