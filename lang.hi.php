5c5b76ee5c853fdfecc4ba7846f5496b786d817f
Vaibhav Bansal <vaibhavbansal23@gmail.com>
2016-10-10 01:46:56 +0530

    fixed new line and slash before quote issues .

--- lang.hi.php
+++ lang.hi.php
@@ -10,41 +10,33 @@ $lang['BEEIMG'] = 'BeeIMG';
 
 // Titles
 $lang['TITLE_HOME'] = 'मुफ्त छवि होस्टिंग और साझा';
-$lang['TITLE_UPLOAD'] = '
-छवियों को अपलोड करें';
-$lang['TITLE_RECENT'] = '
-नये चित्र';
+$lang['TITLE_UPLOAD'] = 'छवियों को अपलोड करें';
+$lang['TITLE_RECENT'] = 'नये चित्र';
 $lang['TITLE_STATS'] = 'आँकड़े';
-$lang['TITLE_TOP'] = '
-शीर्ष 9 छवियों';
+$lang['TITLE_TOP'] = 'शीर्ष 9 छवियों';
 $lang['TITLE_ABOUT'] = 'हमारे बारे में';
 $lang['TITLE_CONTACT'] = 'संपर्क करें';
-$lang['TITLE_TOS'] = '
-सेवा की शर्तें';
+$lang['TITLE_TOS'] = 'सेवा की शर्तें';
 $lang['TITLE_REGISTER'] = 'रजिस्टर';
 $lang['TITLE_LOGIN'] = 'लॉग इन करें';
 $lang['TITLE_DASHBOARD'] = 'डैशबोर्ड';
 
 // Menu
 $lang['MENU_HOME'] = 'घर';
-$lang['MENU_HOME_TITLE'] = '
-हमारा घर';
+$lang['MENU_HOME_TITLE'] = 'हमारा घर';
 $lang['MENU_UPLOAD'] = 'अपलोड';
 $lang['MENU_UPLOAD_TITLE'] = 'छवियों को अपलोड करें';
 $lang['MENU_RECENT'] = 'नये चित्र';
-$lang['MENU_RECENT_TITLE'] = '
-हाल ही में अपलोड की गई छवियों';
+$lang['MENU_RECENT_TITLE'] = 'हाल ही में अपलोड की गई छवियों';
 $lang['MENU_STATS'] = 'आँकड़े';
-$lang['MENU_STATS_TITLE'] = '
-हमारे आँकड़े देखें';
+$lang['MENU_STATS_TITLE'] = 'हमारे आँकड़े देखें';
 $lang['MENU_TOP'] = 'शीर्ष 9';
 $lang['MENU_TOP_TITLE'] = 'सारे टॉप 9 छवियों';
 $lang['MENU_FOLLOW'] = 'का पालन करें';
 
 // Upload Carousel
 $lang['UPLOAD_CAR_LP'] = 'घर';
-$lang['UPLOAD_CAR_1'] = '
-कोई आईपी \ 's अपलोड पर दर्ज किया जाएगा।';
+$lang['UPLOAD_CAR_1'] = 'कोई आईपी \'s अपलोड पर दर्ज किया जाएगा।';
 $lang['UPLOAD_CAR_2'] = 'हम अपने अपलोड उदारवादी।';
 $lang['UPLOAD_CAR_3'] = 'विज्ञापन अनुमति नहीं है।';
 
@@ -53,22 +45,16 @@ $lang['FOOTER_CONTACT'] = 'संपर्क करें';
 $lang['FOOTER_API'] = 'एपीआई';
 $lang['FOOTER_BLOG'] = 'ब्लॉग';
 $lang['FOOTER_TOS'] = 'टीओएस';
-$lang['FOOTER_ABOUT_US'] = '
-हमारे बारे में';
+$lang['FOOTER_ABOUT_US'] = 'हमारे बारे में';
 
 // Stats
-$lang['STATS_HEAD'] = '
-आँकड़े';
+$lang['STATS_HEAD'] = 'आँकड़े';
 
 // Image View Page
-$lang['IMG_VP_IP_HEAD'] = '
-की छवि पूर्वावलोकन';
-$lang['IMG_VP_SHARE_HEAD'] = '
-इस छवि को साझा करें';
+$lang['IMG_VP_IP_HEAD'] = 'की छवि पूर्वावलोकन';
+$lang['IMG_VP_SHARE_HEAD'] = 'इस छवि को साझा करें';
 $lang['IMG_VP_SHARE_DL'] = 'डाउनलोड';
 $lang['IMG_VP_SHARE_DL_BTN_IMG'] = 'छवि';
-$lang['IMG_VP_SHARE_DL_BTN_ZIP'] = '
-ज़िप';
-$lang['IMG_VP_SHARE_BTNS_HEAD'] = '
-शेयर बटन';
+$lang['IMG_VP_SHARE_DL_BTN_ZIP'] = 'ज़िप';
+$lang['IMG_VP_SHARE_BTNS_HEAD'] = 'शेयर बटन';
 ?>
