5eb1b0ab27d3b3b72dcaa208bed0154c8b7605c5
Miyuru Sankalpa <miyurusankalpa@users.noreply.github.com>
2016-10-10 01:31:22 +0530

    updates
    updates and fixes

--- lang.fr.php
+++ lang.fr.php
@@ -9,19 +9,19 @@ $lang = array();
 $lang['BEEIMG'] = 'BeeIMG';
 
 // Titles
-$lang['TITLE_HOME'] = 'Service gratuit d'hébergment et de partage d'images';
+$lang['TITLE_HOME'] = 'Service gratuit d\'hébergment et de partage d\'images';
 $lang['TITLE_UPLOAD'] = 'Téléverser des images';
 $lang['TITLE_RECENT'] = 'Images récentes';
 $lang['TITLE_STATS'] = 'Statistiques';
 $lang['TITLE_TOP'] = 'Top 9 des images';
 $lang['TITLE_ABOUT'] = 'À propos';
 $lang['TITLE_CONTACT'] = 'Contact';
-$lang['TITLE_TOS'] = 'Conditions d'utilisation';
-$lang['TITLE_REGISTER'] = 'S'enregistrer';
+$lang['TITLE_TOS'] = 'Conditions d\'utilisation';
+$lang['TITLE_REGISTER'] = 'S\'enregistrer';
 $lang['TITLE_LOGIN'] = 'Se connecter';
 $lang['TITLE_DASHBOARD'] = 'Tableau de bord';
 
-// Menu
+// Titles
 $lang['MENU_HOME'] = 'Acceuil';
 $lang['MENU_HOME_TITLE'] = 'Notre acceuil';
 $lang['MENU_UPLOAD'] = 'Téléverser';
@@ -38,9 +38,9 @@ $lang['MENU_FOLLOW'] = 'Suivre';
 $lang['UPLOAD_CAR_LP'] = 'Acceuil';
 $lang['UPLOAD_CAR_1'] = 'Aucun IP ne sera enregistré durant le téléversement.';
 $lang['UPLOAD_CAR_2'] = 'Nous modérons vos téléversements.';
-$lang['UPLOAD_CAR_3'] = 'La publicité n'est pas acceptée.';
+$lang['UPLOAD_CAR_3'] = 'La publicité n\'est pas acceptée.';
 
-// Footer
+// Upload Carousel
 $lang['FOOTER_CONTACT'] = 'contact';
 $lang['FOOTER_API'] = 'api';
 $lang['FOOTER_BLOG'] = 'blog';
@@ -51,7 +51,7 @@ $lang['FOOTER_ABOUT_US'] = 'à propos';
 $lang['STATS_HEAD'] = 'Statistiques';
 
 // Image View Page
-$lang['IMG_VP_IP_HEAD'] = 'Aperçu de l'image';
+$lang['IMG_VP_IP_HEAD'] = 'Aperçu de l\'image';
 $lang['IMG_VP_SHARE_HEAD'] = 'Partager cette image';
 $lang['IMG_VP_SHARE_DL'] = 'Télécharger';
 $lang['IMG_VP_SHARE_DL_BTN_IMG'] = 'Image';
--- lang.it.php
+++ lang.it.php
@@ -1,10 +1,14 @@
 <?php
+
 /*------------------
 Language: Italian
 ------------------*/
+
 $lang = array();
+
 $lang['BEEIMG'] = 'BeeIMG';
-// Titoli
+
+// Titles
 $lang['TITLE_HOME'] = 'Free Image Hosting e condivisione';
 $lang['TITLE_UPLOAD'] = 'Caricare immagini';
 $lang['TITLE_RECENT'] = 'Immagini recenti';
@@ -16,7 +20,8 @@ $lang['TITLE_TOS'] = 'Termini di servizio';
 $lang['TITLE_REGISTER'] = 'Registrare';
 $lang['TITLE_LOGIN'] = 'Account di accesso';
 $lang['TITLE_DASHBOARD'] = 'Cruscotto';
-// Dal menu
+
+// Titles
 $lang['MENU_HOME'] = 'Casa';
 $lang['MENU_HOME_TITLE'] = 'La nostra casa';
 $lang['MENU_UPLOAD'] = 'Caricare';
@@ -28,20 +33,24 @@ $lang['MENU_STATS_TITLE'] = 'Visualizza le nostre statistiche';
 $lang['MENU_TOP'] = 'Top 9';
 $lang['MENU_TOP_TITLE'] = 'Visualizza Top 9 immagini';
 $lang['MENU_FOLLOW'] = 'Sequire';
-// Caricare il carosello
+
+// Upload Carousel
 $lang['UPLOAD_CAR_LP'] = 'Casa';
 $lang['UPLOAD_CAR_1'] = 'Nessun mensaje sarà registrato il caricament.';
 $lang['UPLOAD_CAR_2'] = 'Moderiamo i caricamenti.';
 $lang['UPLOAD_CAR_3'] = 'La pubblicità non è consentita.';
-// Piè di pagina
+
+// Upload Carousel
 $lang['FOOTER_CONTACT'] = 'Rivolga';
 $lang['FOOTER_API'] = 'api';
 $lang['FOOTER_BLOG'] = 'blog';
 $lang['FOOTER_TOS'] = 'tos';
 $lang['FOOTER_ABOUT_US'] = 'Chi Siamo';
-// Statistiche
+
+// Stats
 $lang['STATS_HEAD'] = 'Statistiche';
-// Pagina di visualizzazione immagini
+
+// Image View Page
 $lang['IMG_VP_IP_HEAD'] = 'Immagine di anteprima del';
 $lang['IMG_VP_SHARE_HEAD'] = 'Condividi questa immagine';
 $lang['IMG_VP_SHARE_DL'] = 'Scarica';
