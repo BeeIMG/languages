083e222a7431d8afbb837a83160ffc03fb0728b9
Miguel Moreno Gómez <MNXANL@users.noreply.github.com>
2016-10-11 11:38:38 +0200

    Little changes I forgot :')

--- lang.ca.php
+++ lang.ca.php
@@ -15,7 +15,7 @@ $lang['TITLE_CONTACT'] = 'Contacte';
 $lang['TITLE_TOS'] = 'Terminis del Servei';
 $lang['TITLE_REGISTER'] = 'Registre';
 $lang['TITLE_LOGIN'] = 'Login';
-$lang['TITLE_DASHBOARD'] = 'Dashboard';
+$lang['TITLE_DASHBOARD'] = 'Tauler';
 // Menu
 $lang['MENU_HOME'] = 'Inici';
 $lang['MENU_HOME_TITLE'] = 'El Nostre Inici';
@@ -34,10 +34,10 @@ $lang['UPLOAD_CAR_1'] = 'Cap adreça IP es registrarà al pujar.';
 $lang['UPLOAD_CAR_2'] = 'Moderem les vostres pujades.';
 $lang['UPLOAD_CAR_3'] = 'No es permet la publicitat.';
 // Footer
-$lang['FOOTER_CONTACT'] = 'contacte';
-$lang['FOOTER_API'] = 'api';
-$lang['FOOTER_BLOG'] = 'blog';
-$lang['FOOTER_TOS'] = 'tos';
+$lang['FOOTER_CONTACT'] = 'Contacte';
+$lang['FOOTER_API'] = 'API';
+$lang['FOOTER_BLOG'] = 'Blog';
+$lang['FOOTER_TOS'] = 'ToS';
 $lang['FOOTER_ABOUT_US'] = 'Sobre Nosaltres';
 // Stats
 $lang['STATS_HEAD'] = 'Estadístiques';
@@ -47,5 +47,5 @@ $lang['IMG_VP_SHARE_HEAD'] = 'Compartir aquesta Imatge';
 $lang['IMG_VP_SHARE_DL'] = 'Descarrega';
 $lang['IMG_VP_SHARE_DL_BTN_IMG'] = 'Imatge';
 $lang['IMG_VP_SHARE_DL_BTN_ZIP'] = 'Empaquetar en Zip';
-$lang['IMG_VP_SHARE_BTNS_HEAD'] = 'Share Buttons';
+$lang['IMG_VP_SHARE_BTNS_HEAD'] = 'Compartir';
 ?>
