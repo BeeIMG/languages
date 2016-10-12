c51fe1d95fa2cfc24b3691c0f1fc4f2da4c68f19
NateTheRiver <strnadrichard@seznam.cz>
2016-10-13 01:22:34 +0200

    Czech translation
    Czech translation

---
+++ lang.cs.php
@@ -0,0 +1,60 @@
+<?php
+
+/*------------------
+Language: English
+------------------*/
+
+$lang = array();
+
+$lang['BEEIMG'] = 'BeeIMG';
+
+// Titles
+$lang['TITLE_HOME'] = 'Free Image Hosting and Sharing';
+$lang['TITLE_UPLOAD'] = 'Nahrát obrázky';
+$lang['TITLE_RECENT'] = 'Nové obrázky';
+$lang['TITLE_STATS'] = 'Statistiky';
+$lang['TITLE_TOP'] = 'Nejlepších 9 obrázků';
+$lang['TITLE_ABOUT'] = 'O nás';
+$lang['TITLE_CONTACT'] = 'Kontakt';
+$lang['TITLE_TOS'] = 'Podmínky užívání';
+$lang['TITLE_REGISTER'] = 'Registrace';
+$lang['TITLE_LOGIN'] = 'Přihlášení';
+$lang['TITLE_DASHBOARD'] = 'Dashboard';
+
+// Menu
+$lang['MENU_HOME'] = 'Domů';
+$lang['MENU_HOME_TITLE'] = 'Náš domov';
+$lang['MENU_UPLOAD'] = 'Nahrát';
+$lang['MENU_UPLOAD_TITLE'] = 'Nahrát obrázky';
+$lang['MENU_RECENT'] = 'Nové obrázky';
+$lang['MENU_RECENT_TITLE'] = 'Nedávno nahrané obrázky';
+$lang['MENU_STATS'] = 'Statistiky';
+$lang['MENU_STATS_TITLE'] = 'Prohlédněte si naše statistiky';
+$lang['MENU_TOP'] = 'Top 9';
+$lang['MENU_TOP_TITLE'] = 'Našich 9 nejlepších obrázků';
+$lang['MENU_FOLLOW'] = 'Sledovat';
+
+// Upload Carousel
+$lang['UPLOAD_CAR_LP'] = 'Domov';
+$lang['UPLOAD_CAR_1'] = 'Při nahrávání není zaznamenávána IP adresa.';
+$lang['UPLOAD_CAR_2'] = 'Nahrané obrázky prochází naší kontrolou.';
+$lang['UPLOAD_CAR_3'] = 'Není dovoleno použít portál k šíření reklamy.';
+
+// Footer
+$lang['FOOTER_CONTACT'] = 'Kontakt';
+$lang['FOOTER_API'] = 'api';
+$lang['FOOTER_BLOG'] = 'blog';
+$lang['FOOTER_TOS'] = 'tos';
+$lang['FOOTER_ABOUT_US'] = 'o nás';
+
+// Stats
+$lang['STATS_HEAD'] = 'Statistiky';
+
+// Image View Page
+$lang['IMG_VP_IP_HEAD'] = 'Náhled obrázku ';
+$lang['IMG_VP_SHARE_HEAD'] = 'Sdílet obrázek';
+$lang['IMG_VP_SHARE_DL'] = 'Stáhnout';
+$lang['IMG_VP_SHARE_DL_BTN_IMG'] = 'Obrázek';
+$lang['IMG_VP_SHARE_DL_BTN_ZIP'] = 'Zip';
+$lang['IMG_VP_SHARE_BTNS_HEAD'] = 'Tlačítka pro sdílení';
+?>
