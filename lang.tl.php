8be5e9ae9ced7250ace1906e850e947119394a0e
Norvin Yecla <ngydev@outlook.com>
2016-10-15 20:40:39 +0800

    Add Tagalog translation

---
+++ lang.tl.php
@@ -0,0 +1,59 @@
+<?php
+
+/*------------------
+Language: Tagalog
+------------------*/
+
+$lang = array();
+
+$lang['BEEIMG'] = 'BeeIMG';
+
+// Titles
+$lang['TITLE_HOME'] = 'Libreng Image Hosting at Sharing';
+$lang['TITLE_UPLOAD'] = 'Mag-upload ng mga Larawan';
+$lang['TITLE_RECENT'] = 'Mga Bagong Larawan';
+$lang['TITLE_STATS'] = 'Stats';
+$lang['TITLE_TOP'] = 'Top 9 na mga Larawan';
+$lang['TITLE_ABOUT'] = 'Tungkol sa Amin';
+$lang['TITLE_CONTACT'] = 'Kausapin Niyo Kami';
+$lang['TITLE_TOS'] = 'Terms of Service';
+$lang['TITLE_REGISTER'] = 'Magparehistro';
+$lang['TITLE_LOGIN'] = 'Login';
+$lang['TITLE_DASHBOARD'] = 'Dashboard';
+
+// Menu
+$lang['MENU_HOME'] = 'Tahanan';
+$lang['MENU_HOME_TITLE'] = 'Tahanan Natin';
+$lang['MENU_UPLOAD'] = 'Upload';
+$lang['MENU_UPLOAD_TITLE'] = 'Mag-upload ng mga Larawan';
+$lang['MENU_RECENT'] = 'Mga Bagong Larawan';
+$lang['MENU_RECENT_TITLE'] = 'Mga Larawang Kaka-upload Pa Lamang';
+$lang['MENU_STATS'] = 'Stats';
+$lang['MENU_STATS_TITLE'] = 'Tingnan ang aming Stats';
+$lang['MENU_TOP'] = 'Top 9';
+$lang['MENU_TOP_TITLE'] = 'Tingan ang Top 9 na mga Larawan';
+$lang['MENU_FOLLOW'] = 'Sundan';
+
+// Upload Carousel
+$lang['UPLOAD_CAR_LP'] = 'Tahanan';
+$lang['UPLOAD_CAR_1'] = 'Hindi irerecord ang iyong IP kapag ika\'y nag-upload.';
+$lang['UPLOAD_CAR_2'] = 'Aming susubaybayan ang inyong mga upload.';
+$lang['UPLOAD_CAR_3'] = 'Ipinagbabawal ang paglalako (advertising).';
+
+// Footer
+$lang['FOOTER_CONTACT'] = 'contact';
+$lang['FOOTER_API'] = 'api';
+$lang['FOOTER_BLOG'] = 'blog';
+$lang['FOOTER_TOS'] = 'tos';
+$lang['FOOTER_ABOUT_US'] = 'tungkol sa amin';
+
+// Stats
+$lang['STATS_HEAD'] = 'Stats';
+
+// Image View Page
+$lang['IMG_VP_IP_HEAD'] = 'Image Preview ng';
+$lang['IMG_VP_SHARE_HEAD'] = 'I-share ang larawang ito';
+$lang['IMG_VP_SHARE_DL'] = 'I-download';
+$lang['IMG_VP_SHARE_DL_BTN_IMG'] = 'Larawan';
+$lang['IMG_VP_SHARE_DL_BTN_ZIP'] = 'Zip';
+$lang['IMG_VP_SHARE_BTNS_HEAD'] = 'Pindutin para Ishare';
