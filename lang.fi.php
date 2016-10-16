009ee20ddc0aa3d3a0ce7c923a6e660a6b0dad7b
Matias Kainulainen <matias.kainulainen@solita.fi>
2016-10-16 12:47:40 +0300

    Add Finnish translation (#1)

---
+++ lang.fi.php
@@ -0,0 +1,59 @@
+<?php
+
+/*------------------
+Language: Finnish
+------------------*/
+
+$lang = array();
+
+$lang['BEEIMG'] = 'BeeIMG';
+
+// Titles
+$lang['TITLE_HOME'] = 'Ilmainen kuvien tallennus- ja jakamispalvelu';
+$lang['TITLE_UPLOAD'] = 'Lähetä kuvia';
+$lang['TITLE_RECENT'] = 'Viimeisimmät kuvat';
+$lang['TITLE_STATS'] = 'Tilastot';
+$lang['TITLE_TOP'] = 'Top 9 -kuvat';
+$lang['TITLE_ABOUT'] = 'Tietoja meistä';
+$lang['TITLE_CONTACT'] = 'Yhteystiedot';
+$lang['TITLE_TOS'] = 'Käyttöehdot';
+$lang['TITLE_REGISTER'] = 'Rekisteröidy';
+$lang['TITLE_LOGIN'] = 'Kirjaudu sisään';
+$lang['TITLE_DASHBOARD'] = 'Dashboard';
+
+// Menu
+$lang['MENU_HOME'] = 'Koti';
+$lang['MENU_HOME_TITLE'] = 'Koti';
+$lang['MENU_UPLOAD'] = 'Lähetä';
+$lang['MENU_UPLOAD_TITLE'] = 'Lähetä kuvia';
+$lang['MENU_RECENT'] = 'Viimeisimmät kuvat';
+$lang['MENU_RECENT_TITLE'] = 'Viimeksi lisätyt kuvat';
+$lang['MENU_STATS'] = 'Tilastot';
+$lang['MENU_STATS_TITLE'] = 'Katso tilastoja';
+$lang['MENU_TOP'] = 'Top 9';
+$lang['MENU_TOP_TITLE'] = 'Katso top 9 -kuvat';
+$lang['MENU_FOLLOW'] = 'Seuraa';
+
+// Upload Carousel
+$lang['UPLOAD_CAR_LP'] = 'Koti';
+$lang['UPLOAD_CAR_1'] = 'IP:itä ei tallenneta lähetyksen yhteydessä.';
+$lang['UPLOAD_CAR_2'] = 'Moderoimme kaikki lataukset.';
+$lang['UPLOAD_CAR_3'] = 'Mainostus ei ole sallittua.';
+
+// Footer
+$lang['FOOTER_CONTACT'] = 'Yhteystiedot';
+$lang['FOOTER_API'] = 'API';
+$lang['FOOTER_BLOG'] = 'Blogi';
+$lang['FOOTER_TOS'] = 'Käyttöehdot';
+$lang['FOOTER_ABOUT_US'] = 'Tietoja meistä';
+
+// Stats
+$lang['STATS_HEAD'] = 'Stats';
+
+// Image View Page
+$lang['IMG_VP_IP_HEAD'] = 'Esikatselu kuvasta';
+$lang['IMG_VP_SHARE_HEAD'] = 'Jaa tämä kuva';
+$lang['IMG_VP_SHARE_DL'] = 'Lataa';
+$lang['IMG_VP_SHARE_DL_BTN_IMG'] = 'Kuva';
+$lang['IMG_VP_SHARE_DL_BTN_ZIP'] = 'Zip';
+$lang['IMG_VP_SHARE_BTNS_HEAD'] = 'Jakamispainikkeet';
