ef6b7588b9371f5390a4ef9ea80bc4d9de21bb3e
DARDAR SAAD <wmsdardar@gmail.com>
2016-10-29 23:52:19 +0000

    Add Arabic translation
    Add Arabic translation

---
+++ lang.ar.php
@@ -0,0 +1,59 @@
+<?php
+
+/*------------------
+Language: French
+------------------*/
+
+$lang = array();
+
+$lang['BEEIMG'] = 'BeeIMG';
+
+// Titles
+$lang['TITLE_HOME'] = 'خدمة الاستضافة المجانية وتبادل الصور';
+$lang['TITLE_UPLOAD'] = 'إرسال الصور';
+$lang['TITLE_RECENT'] = 'الصور الأخيرة';
+$lang['TITLE_STATS'] = 'إحصائيات';
+$lang['TITLE_TOP'] = 'أهم 9 صور';
+$lang['TITLE_ABOUT'] = 'معلومات عنا';
+$lang['TITLE_CONTACT'] = 'إتصال';
+$lang['TITLE_TOS'] = 'شروط الاستعمال';
+$lang['TITLE_REGISTER'] = 'تسجيل';
+$lang['TITLE_LOGIN'] = 'تسجيل الدخول';
+$lang['TITLE_DASHBOARD'] = 'لوحة القيادة';
+
+// Titles
+$lang['MENU_HOME'] = 'الصفحة الرئيسية';
+$lang['MENU_HOME_TITLE'] = 'صفحتنا الرئسية';
+$lang['MENU_UPLOAD'] = 'تحميل';
+$lang['MENU_UPLOAD_TITLE'] = 'تحميل الصور';
+$lang['MENU_RECENT'] = 'الصور الأخيرة';
+$lang['MENU_RECENT_TITLE'] = 'الصور التي تم تحميلها مؤخرا';
+$lang['MENU_STATS'] = 'إحصائيات';
+$lang['MENU_STATS_TITLE'] = 'عرض الإحصاءات';
+$lang['MENU_TOP'] = 'أفضل 9';
+$lang['MENU_TOP_TITLE'] = 'عرض أفضل 9 صور';
+$lang['MENU_FOLLOW'] = 'إتبع';
+
+// Upload Carousel
+$lang['UPLOAD_CAR_LP'] = 'الصفحة الرئيسية';
+$lang['UPLOAD_CAR_1'] = '.لن يتم تسجيل أي عناوين على تحميل';
+$lang['UPLOAD_CAR_2'] = '.ضبطنا التحميلات';
+$lang['UPLOAD_CAR_3'] = '.لا يسمح بالإعلانات';
+
+// Upload Carousel
+$lang['FOOTER_CONTACT'] = 'اتصال';
+$lang['FOOTER_API'] = 'أ ب ي';
+$lang['FOOTER_BLOG'] = 'مدونة';
+$lang['FOOTER_TOS'] = 'ابتكر';
+$lang['FOOTER_ABOUT_US'] = 'حول';
+
+// Stats
+$lang['STATS_HEAD'] = 'معلومات عنا';
+
+// Image View Page
+$lang['IMG_VP_IP_HEAD'] = 'معاينة الصورة';
+$lang['IMG_VP_SHARE_HEAD'] = 'شارك هذه الصورة';
+$lang['IMG_VP_SHARE_DL'] = 'تحميل';
+$lang['IMG_VP_SHARE_DL_BTN_IMG'] = 'صورة';
+$lang['IMG_VP_SHARE_DL_BTN_ZIP'] = 'Zip';
+$lang['IMG_VP_SHARE_BTNS_HEAD'] = 'أزرار المشاركة';
