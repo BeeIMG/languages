da18191ed95dfdc04ddbf21fe32cd6738884fc6d
Miyuru Sankalpa <miyurusankalpa@users.noreply.github.com>
2017-08-13 20:18:04 +0530

    Update lang.tr.php
    fixes spaces and some untranslated words

--- lang.tr.php
+++ lang.tr.php
@@ -1,12 +1,16 @@
 <?php
+
 /*------------------
 Language: Turkish
 ------------------*/
+
 $lang = array();
+
 $lang['BEEIMG'] = 'BeeIMG';
+
 // Titles
 $lang['TITLE_HOME'] = 'Ücretsiz Resim Hosting ve Paylaşım';
-$lang['TITLE_UPLOAD'] = 'Resim(ler) Yükle';
+$lang['TITLE_UPLOAD'] = 'Resimler Yükle';
 $lang['TITLE_RECENT'] = 'Son Yüklenen Resimler';
 $lang['TITLE_STATS'] = 'İstatistikler';
 $lang['TITLE_TOP'] = 'En Popüler 9 Resim';
@@ -16,31 +20,36 @@ $lang['TITLE_TOS'] = 'Kullanım Şartları';
 $lang['TITLE_REGISTER'] = 'Kayıt Ol';
 $lang['TITLE_LOGIN'] = 'Giriş Yap';
 $lang['TITLE_DASHBOARD'] = 'Panel';
+
 // Menu
 $lang['MENU_HOME'] = 'Anasayfa';
 $lang['MENU_HOME_TITLE'] = 'Anasayfa\'ya Git';
 $lang['MENU_UPLOAD'] = 'Yükle';
-$lang['MENU_UPLOAD_TITLE'] = 'Resim(ler) Yükle';
+$lang['MENU_UPLOAD_TITLE'] = 'Resimler Yükle';
 $lang['MENU_RECENT'] = 'Son Yüklenen Resimler';
 $lang['MENU_RECENT_TITLE'] = 'Son Yüklenen Resimler';
 $lang['MENU_STATS'] = 'İstatistikler';
 $lang['MENU_STATS_TITLE'] = 'İstatistikleri Görüntüle';
-$lang['MENU_TOP'] = 'Top 9';
+$lang['MENU_TOP'] = 'Ilk 9';
 $lang['MENU_TOP_TITLE'] = 'En Popüler 9 Resimi Görüntüle';
 $lang['MENU_FOLLOW'] = 'Takip Et';
+
 // Upload Carousel
 $lang['UPLOAD_CAR_LP'] = 'Anasayfa';
 $lang['UPLOAD_CAR_1'] = 'Yükleme yapılan hiçbir IP adresi kayıt altına alınmayacaktır.';
 $lang['UPLOAD_CAR_2'] = 'Yüklediğiniz resimleri kontrol ediyoruz.';
 $lang['UPLOAD_CAR_3'] = 'Reklamlara izin verilmez.';
+
 // Footer
 $lang['FOOTER_CONTACT'] = 'iletişim';
 $lang['FOOTER_API'] = 'api';
 $lang['FOOTER_BLOG'] = 'blog';
 $lang['FOOTER_TOS'] = 'tos';
 $lang['FOOTER_ABOUT_US'] = 'hakkımızda';
+
 // Stats
-$lang['STATS_HEAD'] = 'Stats';
+$lang['STATS_HEAD'] = 'İstatistikleri';
+
 // Image View Page
 $lang['IMG_VP_IP_HEAD'] = 'Resim Görüntüleme Kapalı';
 $lang['IMG_VP_SHARE_HEAD'] = 'Resmi Paylaş';
@@ -48,4 +57,3 @@ $lang['IMG_VP_SHARE_DL'] = 'İndir';
 $lang['IMG_VP_SHARE_DL_BTN_IMG'] = 'Resim';
 $lang['IMG_VP_SHARE_DL_BTN_ZIP'] = 'Zip';
 $lang['IMG_VP_SHARE_BTNS_HEAD'] = 'Paylaşım Butonları';
-
